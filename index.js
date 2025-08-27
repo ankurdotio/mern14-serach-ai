ba9222d6ddb6405cda217dbdc0d5d5eaa6a206f1
Ankur Prajapati <91510065+Ankur77720@users.noreply.github.com>
2025-08-27 16:57:52 +0530

    Add dotenv support and update .gitignore

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
+import { config } from "dotenv";
 import { GoogleGenAI, Type } from "@google/genai";
 import { tavily } from "@tavily/core"
+import readLine from "readline"
+
+config()
 
 const ai = new GoogleGenAI({
-    apiKey: ""
+    apiKey: process.env.GEMINI_API_KEY
 })
 
 const tvly = tavily({
-    apiKey: ""
+    apiKey: process.env.TAVILY_API_KEY
 })
 
 
@@ -28,25 +32,60 @@ const searchWebTool = {
 const tools = {
     "search_web": async ({ query }) => {
         const response = await tvly.search(query)
-        console.log(response)
+
+        return response.results
     }
 }
 
-ai.models.generateContent({
-    model: "gemini-2.0-flash",
-    contents: "what is the stock price of google today ?",
-    config: {
-        tools: [ {
-            functionDeclarations: [ searchWebTool ]
-        } ]
+// Create an interface for input and output
+const rl = readLine.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const history = []
+
+while (true) {
+
+    history.map(el => console.log)
+
+    if (history[ history.length - 1 ]?.role !== "user") {
+        const message = await new Promise(resolve => rl.question("user : ", resolve))
+        history.push({ role: "user", parts: [ { text: message } ] })
     }
-})
-    .then(response => {
-        response.functionCalls.map(async (call) => {
-            const result = await tools[ call.name ](call.args)
-            console.log(result)
 
-        })
-
+    const aiResponse = await ai.models.generateContent({
+        model: "gemini-2.0-flash",
+        contents: history,
+        config: {
+            tools: [ { functionDeclarations: [ searchWebTool ] } ],
+            systemInstruction: `
+            you can use tools to get more information.
+            `
+        }
     })
 
+
+
+    if (aiResponse.functionCalls && aiResponse.functionCalls[ 0 ]) {
+
+        const result = await tools[ aiResponse.functionCalls[ 0 ].name ](aiResponse.functionCalls[ 0 ].args)
+
+        const content = result.map(r => r.content).join("\n")
+
+        history.push({
+            role: "user",
+            parts: [ {
+                text: `
+                I searched the web for "${aiResponse.functionCalls[ 0 ].args.query}" and found the following information:
+                ${content}
+                `
+            } ]
+        })
+
+    } else {
+        history.push({ role: "model", parts: [ { text: aiResponse.text } ] })
+        console.log("AI: ", aiResponse.text)
+    }
+
+}
